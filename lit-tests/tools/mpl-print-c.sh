76f2a48c2ac758daa63996e2b637ad1e7a1e0764
Patrick Collins <pscollins@google.com>
2026-02-02 10:43:18 -0500

    Easier dumping of pass outputs
    Run as:
    
    ```
    $ OVERRIDE_OUTDIR=/tmp/out tools/mpl-print-c.sh -verbose 3 -diag-pass '.*' tests/sourcemark-1.sml 2>&1 | less
    ```

--- lit-tests/tools/mpl-print-c.sh
+++ lit-tests/tools/mpl-print-c.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 #
 # Wrapper script to compile a binary under `mpl` and print the output
+#
+# Requires that it is called as:
+#   mpl-print-c.sh [OPTIONAL FLAGS] source_file.sml
 set -e
+set -x
 SCRIPT_DIR=$(dirname $(realpath $0))
 MPL=${SCRIPT_DIR}/../../build/bin/mpl
 OUTDIR=$(mktemp -d)
 
-cleanup() {
-    rm -rf "$OUTDIR"
-}
-trap cleanup EXIT
+# Check if OVERRIDE_OUTDIR is provided and non-empty
+if [ -n "$OVERRIDE_OUTDIR" ]; then
+    OUTDIR="$OVERRIDE_OUTDIR"
+    # Create the directory if it doesn't exist
+    mkdir -p "$OUTDIR"
+    echo "Running in ${OUTDIR} (Persisting output)"
+else
+    # Otherwise, generate a temporary directory + cleanup
+    OUTDIR=$(mktemp -d)
+    echo "Running in ${OUTDIR}"
+
+    cleanup() {
+        rm -rf "$OUTDIR"
+    }
+    # Only set the trap if we created the temp directory
+    trap cleanup EXIT
+fi
 
 OUTFILE=${OUTDIR}/out.bin
-${MPL} -output ${OUTFILE} -keep g $@
+# -keep-pass doesn't respect -output, so we need to run in ${OUTDIR}.
+#
+# Extract the last argument and make it absolute
+# ${@: -1} grabs the last arg, ${@:1:$#-1} grabs everything else
+LAST_ARG=$(realpath "${@: -1}")
+OTHER_ARGS=("${@:1:$#-1}")
+cd ${OUTDIR}
+# Pass the earlier arguments as-is and use the absolute path for the last one
+${MPL} -output ${OUTFILE} -keep g "${OTHER_ARGS[@]}" "$LAST_ARG"
 cat ${OUTFILE}.*.c
