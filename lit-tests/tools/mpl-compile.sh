0352663952b3a329deed00656c3ca67c39bba38d
Patrick Collins <pscollins@google.com>
2026-02-08 18:53:46 -0500

    add some comments in mpl-compile.sh

--- lit-tests/tools/mpl-compile.sh
+++ lit-tests/tools/mpl-compile.sh
@@ -10,13 +10,16 @@ set -e
 set -x
 SCRIPT_DIR=$(dirname $(realpath $0))
 MPL=${SCRIPT_DIR}/../../build/bin/mpl
+# Out directory must be the last argument
 OUTDIR="${@: -1}"
+# Input file must be the second to last argument, and we make it absolute here
 INFILE=$(realpath "${@: -2: 1}")
+# Everything beforehand gets passed to the compiler
 COMPILE_ARGS=("${@: 1: $# - 2}")
 
 OUTFILE=${OUTDIR}/out.bin
 # -keep-pass doesn't respect -output, so we need to run in ${OUTDIR}.
 mkdir -p ${OUTDIR}
 cd ${OUTDIR}
-# Pass the earlier arguments as-is and use the absolute path for the last one
+# Pass the earlier arguments as-is and use the absolute path for the input file
 ${MPL} -output ${OUTFILE} "${COMPILE_ARGS[@]}" "$INFILE"
